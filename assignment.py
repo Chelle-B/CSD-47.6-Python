e32ffec4855f4485cb28f05d64ee0182b2b468b7
Chelle-B <maaboateng@005st.ug.edu.gh>
2025-12-10 16:30:05 +0000

    Revised Python Assignment

--- assignment.py
+++ assignment.py
@@ -4,14 +4,16 @@
 songs = ["Song1","Song2","Song3","Song4","Song5"]
 
 #Question 2
-top_artists = ("Ama","Kofi","Kwame","Esi","Abena")
+top_artists = ("Ama","Kofi","Kwame","Esi")
 
 #Question 3
 my_music = [songs, top_artists]
+
+#Question 4
 print(len(my_music))
 
 #Question 5 
-print(songs[3])
+print(songs[2])
 
 #Question 6 
 songs[0] = "Perfect"
