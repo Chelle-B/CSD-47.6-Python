d10e662bb2f57df215934c27f2f185181eb722f5
Chelle-B <maaboateng@005st.ug.edu.gh>
2025-12-10 16:26:36 +0000

    Assignment Commit

---
+++ assignment.py
@@ -0,0 +1,23 @@
+#Questions 
+#Question 1 
+
+songs = ["Song1","Song2","Song3","Song4","Song5"]
+
+#Question 2
+top_artists = ("Ama","Kofi","Kwame","Esi","Abena")
+
+#Question 3
+my_music = [songs, top_artists]
+print(len(my_music))
+
+#Question 5 
+print(songs[3])
+
+#Question 6 
+songs[0] = "Perfect"
+
+#Question 7 
+songs.append("Blinding Lights")                             
+
+#Question 8 
+print(songs)
