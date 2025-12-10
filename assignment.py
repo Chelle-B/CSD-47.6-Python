62ee003dba511e8fd540ed1ce1261e84b7801d9b
Chelle-B <maaboateng@005st.ug.edu.gh>
2025-12-10 16:43:27 +0000

    Revised Assignment Python

--- assignment.py
+++ assignment.py
@@ -1,25 +1,25 @@
 #Questions 
 #Question 1 
 
-songs = ["Song1","Song2","Song3","Song4","Song5"]
+playlists = ["So will I ","Closer","Rahama","D2D","Lacrea"]
 
 #Question 2
-top_artists = ("Ama","Kofi","Kwame","Esi")
+top_artists = ("Hilsong","Nina Shezz","Kwame","Esi")
 
 #Question 3
-my_music = [songs, top_artists]
+my_music = [playlists, top_artists]
 
 #Question 4
 print(len(my_music))
 
 #Question 5 
-print(songs[2])
+print(playlists[2])
 
 #Question 6 
-songs[0] = "Perfect"
+playlists[0] = "Perfect"
 
 #Question 7 
-songs.append("Blinding Lights")                             
+playlists.append("Blinding Lights")                             
 
 #Question 8 
-print(songs)
+print(playlists )
